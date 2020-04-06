4fd9f0e3827f6c94ef191271eeddb9cefa78f753
jacolee17 <anivilds@gmail.com>
2020-04-06 19:02:27 -0400

    added opening chest

--- Assets/ChestHandler.cs
+++ Assets/ChestHandler.cs
@@ -9,26 +9,61 @@ public class ChestHandler : MonoBehaviour
     public List<Sprite> gemImages = new List<Sprite>();
     public float basicGemValue;
     public Transform money;
+    public int chestIniCount;
 
-    GameObject closedChest, openedChest, gemimage, gemname, gemvalue, button;
+    GameObject closedChest, openedChest, gemimage, gemname, gemvalue, button, chestcount;
 
     bool isRevealed = false;
+    bool isOpeningChest = false;
     float gemValue;
+    int currentChest;
 
     // Start is called before the first frame update
     void Start()
     {
-         closedChest = transform.GetChild(0).gameObject;
-         openedChest = transform.GetChild(1).gameObject;
-         gemimage = transform.GetChild(2).gameObject;
-         gemname = transform.GetChild(3).gameObject;
-         gemvalue = transform.GetChild(4).gameObject;
-         button = transform.GetChild(5).gameObject;
+       closedChest = transform.GetChild(0).gameObject;
+       openedChest = transform.GetChild(1).gameObject;
+       gemimage = transform.GetChild(2).gameObject;
+       gemname = transform.GetChild(3).gameObject;
+       gemvalue = transform.GetChild(4).gameObject;
+       button = transform.GetChild(5).gameObject;
+       chestcount = transform.GetChild(6).gameObject;
 
-         openedChest.SetActive(false);
-         gemimage.SetActive(false);
-         gemname.SetActive(false);
-         gemvalue.SetActive(false);
+       closedChest.SetActive(false);
+       openedChest.SetActive(false);
+       gemimage.SetActive(false);
+       gemname.SetActive(false);
+       gemvalue.SetActive(false);
+       button.SetActive(false);
+
+       currentChest = chestIniCount;
+       chestcount.GetComponent<Text>().text = currentChest.ToString();
+    }
+
+    void reset() {
+      button.GetComponentInChildren<Text>().text = "Open chest";
+      button.SetActive(false);
+      openedChest.SetActive(false);
+      gemimage.SetActive(false);
+      gemname.SetActive(false);
+      gemvalue.SetActive(false);
+      isRevealed = false;
+      isOpeningChest = false;
+    }
+
+    public void openChest(){
+       if (currentChest==0) return;
+       if (isOpeningChest) return;
+       button.SetActive(true);
+       closedChest.SetActive(true);
+       currentChest--;
+       chestcount.GetComponent<Text>().text = currentChest.ToString();
+       isOpeningChest = true;
+    }
+
+    public void addChest(int byAmount) {
+      currentChest += byAmount;
+      chestcount.GetComponent<Text>().text = currentChest.ToString();
     }
 
     public void buttonClicked() {
@@ -50,7 +85,7 @@ public class ChestHandler : MonoBehaviour
       }
       else {
         money.gameObject.GetComponent<MoneyCounter>().addMoney(gemValue);
-        Destroy(gameObject,0.01f);
+        reset();
       }
     }
 }
